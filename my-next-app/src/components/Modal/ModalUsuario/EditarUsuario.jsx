b0dd4b25876bfbedc86d09618b82e23d2e619a3c
Otro <gonzalezmartinnatanael@gmail.com>
2025-02-04 12:33:38 -0300

    comit

--- my-next-app/src/components/Modal/ModalUsuario/EditarUsuario.jsx
+++ my-next-app/src/components/Modal/ModalUsuario/EditarUsuario.jsx
@@ -88,7 +88,7 @@ function ModalEditarUsuario({ isOpen, onRequestClose, usuario, notificacion }) {
 
                         <select
                             onChange={(e) => setRole(e.target.value)}
-                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:outline-none"
+                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
                             defaultValue=""
                         >
                             <option value="" disabled>Seleccione un rol</option>
